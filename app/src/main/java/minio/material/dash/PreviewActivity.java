b9b89948ab067929ad10b51d5204e35d1e18f96c
Patrick J <patrick.pddstudio@googlemail.com>
2016-02-05 09:53:14 +0100

    Added ZooperUtils support

--- app/src/main/java/minio/material/dash/PreviewActivity.java
+++ app/src/main/java/minio/material/dash/PreviewActivity.java
@@ -1,16 +1,23 @@
 package minio.material.dash;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import com.pddstudio.zooperutils.SimpleWidgetCallback;
+import com.pddstudio.zooperutils.ZooperUtils;
+
+import java.io.IOException;
+import java.util.List;
 
 import minio.material.dash.adapters.PreviewAdapter;
 
 /**
  * Created by sasikanth on 05/02/16.
  */
-public class PreviewActivity extends AppCompatActivity {
+public class PreviewActivity extends AppCompatActivity implements SimpleWidgetCallback {
 
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
@@ -20,12 +27,25 @@ public class PreviewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.preview_main);
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        layoutManager = new GridLayoutManager(this, 2);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setHasFixedSize(true);
+        try {
+            ZooperUtils.init(this);
+            ZooperUtils.getInstance().registerSimpleWidgetCallback(this);
+        } catch (IOException io) {
+            io.printStackTrace();
+            finish();
+        }
+
+
 
 
     }
 
+    @Override
+    public void onFinishedLoading(List<Bitmap> images) {
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(ZooperUtils.getInstance().createSimpleWidgetAdapter(images, true));
+        recyclerView.setHasFixedSize(true);
+    }
 }
